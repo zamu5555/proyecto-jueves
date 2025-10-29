0f019dddbe438c0fdb80b29bad7df538ba249a77
Samuel Diaz <samueld2604@gmail.com>
2025-10-29 00:07:47 -0500

    deletemouse

--- mouse.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-  const trail = document.createElement("div");
-  trail.className = "trail";
-  trail.style.left = `${e.pageX - 5}px`;
-  trail.style.top = `${e.pageY - 5}px`;
-  document.body.appendChild(trail);
-
-  setTimeout(() => trail.remove(), 1000);
-});
